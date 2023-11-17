78668683f6afa4d3a136d6c0b40799204c5a8f9d
Sobuj <fisobuj@gmail.com>
2023-11-16 20:52:40 -0500

    switch

---
+++ Season.java
@@ -0,0 +1,61 @@
+package switchStatement;
+
+import java.util.Scanner;
+
+public class Season {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String st=sc.next();
+		switch(st) {
+		case "January":{
+			System.out.println("This is Winter time");
+			break;
+		}
+		case "Febeary":{
+			System.out.println("This is Winter time");
+			break;
+		}
+		case "March":{
+			System.out.println("This is Spring time");
+			break;
+		}
+		case "April":{
+			System.out.println("This is Spring time");
+			break;
+		}
+		case "May":{
+			System.out.println("This is Spring time");
+			break;
+		}
+		case "June":{
+			System.out.println("This is Summer time");
+			break;
+		}
+		case "July":{
+			System.out.println("This is Summer time");
+			break;
+		}
+		case "August":{
+			System.out.println("This is Summer time");
+			break;
+		}
+		case "September":{
+			System.out.println("This is Ort time");
+			break;
+		}
+		case "October":{
+			System.out.println("This is Ort time");
+			break;
+		}
+		case "November":{
+			System.out.println("This is Ort time");
+			break;
+		}
+		case "December":{
+			System.out.println("This is Winter time");
+		}
+		}
+	}
+
+}
